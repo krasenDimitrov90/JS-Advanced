2f00e612d479e63709a46820b67b8f68424cb5a3
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-06-19 23:17:38 +0300

    New tasks

--- 11. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
+++ 11. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    
+    let inputEmailElement = document.getElementById('email');
+    
+    inputEmailElement.addEventListener('change' , function (e) {
+        if (!(/^[a-z]+@[a-z]+\.[a-z]+$/g).test(inputEmailElement.value)) {
+            inputEmailElement.setAttribute('class' , 'error')
+        } else {
+            inputEmailElement.removeAttribute('class')
+        }
+    })
 }
--- 11. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ 11. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-   //TODO...
+
+   let addButonsElements = document.querySelectorAll('.add-product');
+   let textAreaElement = document.querySelector('textarea')
+   let products = {};
+   let addButonsElementsArr = Array.from(addButonsElements);
+
+   addButonsElementsArr.forEach(el => {
+      el.addEventListener('click', function (e) {
+         let productTypeElement = e.currentTarget.parentNode.parentNode.querySelector('.product-title');
+         let productPricesElement = e.currentTarget.parentNode.parentNode.querySelector('.product-line-price');
+         let product = productTypeElement.textContent;
+         let price = Number(productPricesElement.textContent);
+         
+         if (!products.hasOwnProperty(product)) {
+            products[product] = 0;
+         }
+         products[product] += price;
+         textAreaElement.textContent += `Added ${product} for ${price.toFixed(2)} to the cart.\n`
+         
+      })
+   })
+
+   let chekOutButtonElement = document.querySelector('.checkout')
+   chekOutButtonElement.addEventListener('click' , function (e) {
+
+      let totalPrice = Object.values(products).reduce((acc , x) => acc + x ,0)
+      let keys = Object.keys(products);
+      let result = `You bought ${keys.join(', ')} for ${totalPrice.toFixed(2)}.`;
+      textAreaElement.textContent += result;
+      chekOutButtonElement.setAttribute('disabled', true);
+      addButonsElementsArr.forEach(el => el.setAttribute('disabled', true));
+   })
 }
