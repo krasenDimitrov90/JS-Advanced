802732019919f39c0f573014725f7fe953db993f
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-06-19 19:19:00 +0300

    New task

--- 11. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
+++ 11. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    let inputElements = document.querySelectorAll('div > div input[type="text"]')
+    
+    let mouseFocusedHandler = function (e) {
+        e.currentTarget.parentNode.setAttribute('class' , 'focused')
+    }
+
+    let mouseUnFocucedHandler = (e) => {
+        e.currentTarget.parentNode.removeAttribute('class')
+    }
+
+    for (const el of inputElements) {
+        el.addEventListener('focus' , mouseFocusedHandler)
+        el.addEventListener('blur' , mouseUnFocucedHandler)
+    }
 }
