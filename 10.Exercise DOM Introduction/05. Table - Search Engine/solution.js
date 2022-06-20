42f85a08249cf93466de9451a4fd6c278b91e206
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-06-20 21:18:30 +0300

    New tasks

--- 10.Exercise DOM Introduction/05. Table - Search Engine/solution.js
+++ 10.Exercise DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,21 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
 
+      let trRowElements = document.querySelectorAll('tbody tr');
+      for (let tr of trRowElements) {
+         tr.removeAttribute('class');
+      }
+
+      let tdRowsElements = document.querySelectorAll('tbody tr td');
+      let searchInputElement = document.getElementById('searchField');
+
+      let searchedText = searchInputElement.value;
+      searchInputElement.value = '';
+      for (let tdRow of tdRowsElements) {
+         if (tdRow.textContent.includes(searchedText)) {
+            tdRow.parentNode.setAttribute('class', 'select')
+         }
+      }
    }
 }
--- 10.Exercise DOM Introduction/06. Format the Text/solution.js
+++ 10.Exercise DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO
+
+  let textAreaInputElement = document.getElementById('input');
+  let outputElement = document.getElementById('output');
+
+  let text = textAreaInputElement.value;
+  let splitText = text.split('.').filter(s => s !== '');
+
+  while (splitText.length > 0) {
+
+    let p = document.createElement('p');
+    p.textContent = splitText.splice(0, 3).join('. ') + '.';
+    outputElement.appendChild(p)
+  }
 }
