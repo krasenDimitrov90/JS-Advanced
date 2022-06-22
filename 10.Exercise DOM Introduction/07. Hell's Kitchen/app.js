065f081db959384e3faf43fddea0ba1ac26c1da1
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-06-22 08:27:52 +0300

    New tasks

--- 10.Exercise DOM Introduction/07. Hell's Kitchen/app.js
+++ 10.Exercise DOM Introduction/07. Hell's Kitchen/app.js
@@ -1,8 +1,54 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+   function onClick() {
+      let inputElements = document.querySelector('#inputs textarea');
+      let resturantsData = JSON.parse(inputElements.value);
+      let resturants = {};
+
+      for (let line of resturantsData) {
+         let tokens = line.match(/\w+/g);
+         let resturant = tokens.shift();
+         if (!resturants.hasOwnProperty(resturant)) {
+            resturants[resturant] = { avarageSalary: 0 };
+         }
+         let tokensL = tokens.length;
+         for (let i = 0; i < tokensL; i += 2) {
+            let worker = tokens[i];
+            let salary = Number(tokens[i + 1]);
+            resturants[resturant][worker] = salary;
+            resturants[resturant].avarageSalary += salary;
+         }
+      }
+
+      let resturantsKeys = Object.keys(resturants);
+      let bestResturant = '';
+      let bestRestAvrgSalary = 0;
+      for (let resturant of resturantsKeys) {
+         let currentAvarageSalary = resturants[resturant].avarageSalary;
+         if (currentAvarageSalary > bestRestAvrgSalary) {
+            bestRestAvrgSalary = currentAvarageSalary;
+            bestResturant = resturant;
+         }
+      }
+
+      let sorted = Object.entries(resturants[bestResturant])
+         .splice(1)
+         .sort((a, b) => {
+            return b[1] - a[1];
+         })
+      let bestSalary = sorted[0][1];
+      bestRestAvrgSalary /= sorted.length
+      let bestResturantPElement = document.querySelector('#bestRestaurant p')
+      bestResturantPElement.textContent = `Name: ${bestResturant} Average Salary: ${bestRestAvrgSalary.toFixed(2)} Best Salary: ${bestSalary.toFixed(2)}`
+
+
+      let result = '';
+      for (let [worker, workerSalary] of sorted) {
+         result += `Name: ${worker} With Salary: ${workerSalary} `
+      }
+      result.trim();
+      let workersPElement = document.querySelector('#workers p')
+      workersPElement.textContent = result;
    }
 }
--- 10.Exercise DOM Introduction/08. Generate Report/report.js
+++ 10.Exercise DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,22 @@
 function generateReport() {
-    //TODO
+    
+    let report = [];
+    let checkBoxesElements = document.querySelectorAll('thead tr th input');
+    let rowsElements = document.querySelectorAll('tbody tr');
+
+    let checkBoxesElementsL = checkBoxesElements.length;
+    let rowsElementsL = rowsElements.length;
+
+    for (let i = 0; i < rowsElementsL; i++) {
+
+        let data = {};
+        for (let j = 0; j < checkBoxesElementsL; j++) {
+           if (checkBoxesElements[j].checked) {
+              data[checkBoxesElements[j].name] = rowsElements[i].children[j].textContent
+           }  
+        }
+        report.push(data);
+    }
+    
+    document.getElementById('output').textContent = JSON.stringify(report)
 }
