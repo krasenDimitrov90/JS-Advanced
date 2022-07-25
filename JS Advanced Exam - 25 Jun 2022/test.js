da00d1a6751afe13b036bb8fee1307f04ee70057
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-07-25 07:59:55 +0300

    New

--- JS Advanced Exam - 25 Jun 2022/test.js
+++ JS Advanced Exam - 25 Jun 2022/test.js
@@ -1,6 +1,13 @@
-let str = ''
-if (str) {
-    console.log(str);
-} else {
-    console.log('no');
+function humanReadable(seconds) {
+
+    let hours = Math.floor(seconds / 3600)
+    let min = Math.min(Math.floor((seconds - hours * 3600) / 60), 59)
+    let leftSec = Math.min(Math.floor(seconds - min * 60 - hours * 3600), 59)
+
+    let result = hours.toString().padStart(2, '0');
+    result += ':' + min.toString().padStart(2, '0');
+    result += ':' + leftSec.toString().padStart(2, '0');
+    return result;
 }
+
+console.log(humanReadable(86399));
